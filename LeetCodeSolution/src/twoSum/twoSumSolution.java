43de46e07f8c0775cce2b53972ffb92cea5f5074
Devesh <Tyagi77@outlook.vom>
2020-05-31 15:52:01 +0530

    Two Sum

---
+++ LeetCodeSolution/src/twoSum/twoSumSolution.java
@@ -0,0 +1,20 @@
+package twoSum;
+
+public class twoSumSolution {
+	 public int[] twoSum(int[] nums, int target) {
+	        Map<Integer,Integer> map = new HashMap<>();
+	        int[] output = new int[2];
+	        for(int i=0;i<nums.length;i++){
+	            if(map.containsKey(target - nums[i])){
+	                output[0] = map.get(target - nums[i]);
+	                output[1] = i;
+	                return output;
+	                
+	                
+	            }else{
+	                map.put(nums[i],i);
+	            }
+	        }
+	        return output;
+	    }
+}
